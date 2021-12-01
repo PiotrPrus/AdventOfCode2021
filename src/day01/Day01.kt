192512877ff61114fd4eceb570d7596a6d990861
piotrprus <piotr.prus@schibsted.com>
2021-12-01 23:47:59 +0100

    change the function calls as it was in template

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,41 +1,31 @@
 package day01
 
 fun main() {
-    // test if implementation meets criteria from the description, like:
+    fun part1(input: List<String>): Int {
+        val list = input.map { it.toInt() }
+        var increased = 0
+        (1..list.lastIndex).forEach { index ->
+            if (list[index] > list[index.minus(1)]) increased++
+        }
+        return increased
+    }
+    fun part2(input: List<String>): Int {
+        val list = input.map { it.toInt() }
+        var increased = 0
+        (0..list.lastIndex.minus(3)).forEach { index ->
+            val currentGroup = list[index] + list[index.plus(1)] + list[index.plus(2)]
+            val nextGroup = list[index.plus(1)] + list[index.plus(2)] + list[index.plus(3)]
+            if (nextGroup > currentGroup) increased++
+        }
+        return increased
+    }
+
+    // test
     val testInput = readInput("day01/Day01_test")
-    check(part1(testInput) == 6)
-    val testList = testInput.map { it.toInt() }
-    val testIncreased = countIncreased(testList)
-    assert(testIncreased == 4)
-    val testIncreasedByGroup = countIncreasedByGroup(testList)
-    assert(testIncreasedByGroup == 3)
+    assert(part1(testInput) == 4)
+    assert(part2(testInput) == 3)
 
     val input = readInput("day01/Day01")
-    countIncreased(input.map { it.toInt() })
-    countIncreasedByGroup(input.map { it.toInt() })
-}
-
-// Day 1, part 2
-private fun countIncreasedByGroup(list: List<Int>): Int {
-    var increased = 0
-    (0..list.lastIndex.minus(3)).forEach { index ->
-        val currentGroup = list[index] + list[index.plus(1)] + list[index.plus(2)]
-        val nextGroup = list[index.plus(1)] + list[index.plus(2)] + list[index.plus(3)]
-        if (nextGroup > currentGroup) increased++
-    }
-    println("Increased by group: $increased")
-    return increased
-}
-
-private fun countIncreased(list: List<Int>): Int {
-    var increased = 0
-    (1..list.lastIndex).forEach { index ->
-        if (list[index] > list[index.minus(1)]) increased++
-    }
-    println("Increased result: $increased")
-    return increased
-}
-
-fun part1(input: List<String>): Int {
-    return input.size
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
 }
