e8fe705d89845c46b378852f878517c9e767b072
piotrprus <piotr.prus@schibsted.com>
2021-12-01 11:21:54 +0100

    puzzle 2 solved

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -7,9 +7,24 @@ fun main() {
     val testList = testInput.map { it.toInt() }
     val testIncreased = countIncreased(testList)
     assert(testIncreased == 4)
+    val testIncreasedByGroup = countIncreasedByGroup(testList)
+    assert(testIncreasedByGroup == 3)
 
     val input = readInput("day01/Day01")
     countIncreased(input.map { it.toInt() })
+    countIncreasedByGroup(input.map { it.toInt() })
+}
+
+// Day 1, part 2
+private fun countIncreasedByGroup(list: List<Int>): Int {
+    var increased = 0
+    (0..list.lastIndex.minus(3)).forEach { index ->
+        val currentGroup = list[index] + list[index.plus(1)] + list[index.plus(2)]
+        val nextGroup = list[index.plus(1)] + list[index.plus(2)] + list[index.plus(3)]
+        if (nextGroup > currentGroup) increased++
+    }
+    println("Increased by group: $increased")
+    return increased
 }
 
 private fun countIncreased(list: List<Int>): Int {
