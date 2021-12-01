ed657b8f4177f51838a98f5d344cc2a65ae41483
piotrprus <piotr.prus@schibsted.com>
2021-12-01 11:02:50 +0100

    puzzle 1 solved

--- src/day01/Day01.kt
+++ src/day01/Day01.kt
@@ -1,19 +1,26 @@
 package day01
 
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("day01/Day01_test")
+    check(part1(testInput) == 6)
+    val testList = testInput.map { it.toInt() }
+    val testIncreased = countIncreased(testList)
+    assert(testIncreased == 4)
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    val input = readInput("day01/Day01")
+    countIncreased(input.map { it.toInt() })
+}
+
+private fun countIncreased(list: List<Int>): Int {
+    var increased = 0
+    (1..list.lastIndex).forEach { index ->
+        if (list[index] > list[index.minus(1)]) increased++
+    }
+    println("Increased result: $increased")
+    return increased
+}
+
+fun part1(input: List<String>): Int {
+    return input.size
 }
