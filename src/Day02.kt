a30782e453e9743f1ddfc9188280bd6d7d5bbb82
piotrprus <piotr.prus@schibsted.com>
2021-12-02 16:02:02 +0100

    day2 part1 completed

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,6 +1,19 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val converted = input.map {
+            val split = it.split(" ")
+            split[0] to split[1].toInt()
+        }
+        var forward = 0
+        var depth = 0
+        converted.forEach { pair ->
+            when(pair.first) {
+                "forward" -> forward = forward.plus(pair.second)
+                "down" -> depth = depth.plus(pair.second)
+                "up" -> depth = depth.minus(pair.second)
+            }
+        }
+        return forward.times(depth)
     }
     fun part2(input: List<String>): Int {
         return 0
@@ -8,6 +21,7 @@ fun main() {
 
     // test
     val testInput = readInput("Day02_test")
+    assert(part1(testInput) == 14)
 
     val input = readInput("Day02")
     println("Part 1: ${part1(input)}")
