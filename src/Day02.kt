309f7292d90bde42b77111982bfe0356a5abf5fb
piotrprus <piotr.prus@schibsted.com>
2021-12-03 14:20:05 +0100

    day2 part2 completed

--- src/Day02.kt
+++ src/Day02.kt
@@ -16,12 +16,30 @@ fun main() {
         return forward.times(depth)
     }
     fun part2(input: List<String>): Int {
-        return 0
+        val converted = input.map {
+            val split = it.split(" ")
+            split[0] to split[1].toInt()
+        }
+        var horizontal = 0
+        var depth = 0
+        var aim = 0
+        converted.forEach { pair ->
+            when(pair.first) {
+                "forward" -> {
+                    horizontal = horizontal.plus(pair.second)
+                    depth += pair.second * aim
+                }
+                "down" -> aim = aim.plus(pair.second)
+                "up" -> aim = aim.minus(pair.second)
+            }
+        }
+        return horizontal * depth
     }
 
     // test
     val testInput = readInput("Day02_test")
     assert(part1(testInput) == 14)
+    assert(part2(testInput) == 252)
 
     val input = readInput("Day02")
     println("Part 1: ${part1(input)}")
