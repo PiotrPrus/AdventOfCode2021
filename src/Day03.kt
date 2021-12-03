1dd84d3c02e59ab3fa7ee4c8843b4faba213a0cd
piotrprus <piotr.prus@schibsted.com>
2021-12-03 14:22:09 +0100

    day3 initial commit

---
+++ src/Day03.kt
@@ -0,0 +1,17 @@
+fun main() {
+    fun part1(input: List<String>): Int {
+        return 0
+    }
+    fun part2(input: List<String>): Int {
+        return 0
+    }
+
+    // test
+    val testInput = readInput("Day03_test")
+    assert(part1(testInput) == 0)
+    assert(part2(testInput) == 0)
+
+    val input = readInput("Day03")
+    println("Part 1: ${part1(input)}")
+    println("Part 2: ${part2(input)}")
+}
