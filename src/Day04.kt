8f460c29250a108db65e6dc961399a1f1a7ad526
piotrprus <piotr.prus@schibsted.com>
2021-12-08 15:55:49 +0100

    day4 part2 solution

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,26 +1,7 @@
 fun main() {
     fun part1(input: List<String>): Int {
         val draws = input.first().split(",").map { it.toInt() }
-
-        val boards = input.drop(1).chunked(6)
-            .map { group -> group.drop(1) }.map { board ->
-                board.map { line ->
-                    line.split("  ", " ")
-                        .filter { it.isNotBlank() } // Leading whitespace
-                        .map { it.toInt() }
-                }
-            }
-
-        val list: List<Board> = boards.map { board ->
-            val points = mutableListOf<BoardField>()
-            board.forEachIndexed { indexX, column ->
-                column.forEachIndexed { indexY, value ->
-                    points.add(BoardField(positionX = indexX, positionY = indexY, value = value))
-                }
-            }
-            Board(points = points)
-        }
-
+        val list = convertToListOfBoards(input)
 
         draws.forEach { draw ->
             list.onEach { board ->
@@ -39,17 +20,62 @@ fun main() {
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        val draws = input.first().split(",").map { it.toInt() }
+        val boards = convertToListOfBoards(input)
+        val completedBoards = mutableListOf<Board>()
+        var lastDraw = 0
+
+        draws.forEach { draw ->
+            boards.onEach { board ->
+                if (!board.completed) {
+                    board.points.forEach {
+                        if (draw == it.value) {
+                            it.selected = true
+                        }
+                    }
+                    if (board.completed) {
+                        completedBoards.add(board)
+                        lastDraw = draw
+                    }
+                }
+            }
+        }
+
+        val lastBoard = completedBoards.last().points.sumOf { if (!it.selected) it.value else 0 }
+
+        return lastBoard.times(lastDraw)
     }
 
     // test
     val testInput = readInput("Day04_test")
     assert(part1(testInput) == 4512)
-    assert(part2(testInput) == 0)
+    assert(part2(testInput) == 1924)
 
     val input = readInput("Day04")
     println("Part 1: ${part1(input)}")
     println("Part 2: ${part2(input)}")
+}
+
+private fun convertToListOfBoards(input: List<String>): List<Board> {
+    val boards = input.drop(1).chunked(6)
+        .map { group -> group.drop(1) }.map { board ->
+            board.map { line ->
+                line.split("  ", " ")
+                    .filter { it.isNotBlank() } // Leading whitespace
+                    .map { it.toInt() }
+            }
+        }
+
+    val list: List<Board> = boards.map { board ->
+        val points = mutableListOf<BoardField>()
+        board.forEachIndexed { indexX, column ->
+            column.forEachIndexed { indexY, value ->
+                points.add(BoardField(positionX = indexX, positionY = indexY, value = value))
+            }
+        }
+        Board(points = points)
+    }
+    return list
 }
 
 data class Board(
