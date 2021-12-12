f578d98ea2518e1b3c432ad13d573388ed0e9d46
piotrprus <piotr.prus@schibsted.com>
2021-12-12 22:03:28 +0100

    day5 part1 completed

--- src/Day05.kt
+++ src/Day05.kt
@@ -1,6 +1,42 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 0
+        val lines = input.map { line ->
+            val twoPoints = line.split(" -> ")
+            val startPoint = twoPoints.first().split(",").map { it.toInt() }
+            val endPoint = twoPoints[1].split(",").map { it.toInt() }
+            Line(
+                start = Point(x = startPoint.first(), y = startPoint[1]),
+                end = Point(x = endPoint.first(), y = endPoint[1])
+            )
+        }
+
+        val points = mutableListOf<Point>()
+
+        lines.filter { it.isHorizontalOrVertical }.forEach { line ->
+            if (line.start.x == line.end.x) {
+                 if (line.start.y > line.end.y) {
+                     (line.start.y.downTo(line.end.y)).forEach {
+                         points.add(Point(x = line.start.x, y = it))
+                     }
+                 } else {
+                     (line.start.y..line.end.y).forEach {
+                         points.add(Point(x = line.start.x, y = it))
+                     }
+                 }
+            } else {
+                if (line.start.x > line.end.x) {
+                    (line.start.x.downTo(line.end.x)).forEach {
+                        points.add(Point(x = it, y = line.start.y))
+                    }
+                } else {
+                    (line.start.x..line.end.x).forEach {
+                        points.add(Point(x = it, y = line.start.y))
+                    }
+                }
+            }
+        }
+
+        return points.groupingBy { it }.eachCount().filter { it.value > 1 }.size
     }
 
     fun part2(input: List<String>): Int {
@@ -9,10 +45,22 @@ fun main() {
 
     // test
     val testInput = readInput("Day05_test")
-    assert(part1(testInput) == 0)
+    assert(part1(testInput) == 5)
     assert(part2(testInput) == 0)
 
     val input = readInput("Day05")
     println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+//    println("Part 2: ${part2(input)}")
 }
+
+data class Line(
+    val start: Point,
+    val end: Point
+) {
+    val isHorizontalOrVertical: Boolean
+        get() {
+            return start.x == end.x || start.y == end.y
+        }
+}
+
+data class Point(val x: Int, val y: Int)
