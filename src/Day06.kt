bf3b0eb3a0c93f18470d4046b4da6272f861981c
piotrprus <piotr.prus@schibsted.com>
2021-12-14 09:28:47 +0100

    day6 part1 solved

--- src/Day06.kt
+++ src/Day06.kt
@@ -1,6 +1,24 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return 5934
+        val fish = input.first().split(",").map { Lanternfish(timer = it.toInt()) }.toMutableList()
+        var newFish = 0
+        repeat(80) { _ ->
+            if (newFish > 0) {
+                fish.addAll(List(newFish) { Lanternfish() })
+                newFish = 0
+            }
+            fish.onEach {
+                if (it.timer == 0) {
+                    it.reset()
+                    newFish++
+                } else {
+                    it.timer = it.timer.minus(1)
+                }
+            }
+        }
+
+        fish.addAll(List(newFish) { Lanternfish() })
+        return fish.size
     }
 
     fun part2(input: List<String>): Int {
@@ -8,11 +26,21 @@ fun main() {
     }
 
     // test
-    val testInput = readInput("Day05_test")
+    val testInput = readInput("Day06_test")
     assert(part1(testInput) == 5934)
     assert(part2(testInput) == 0)
 
-    val input = readInput("Day05")
+    val input = readInput("Day06")
     println("Part 1: ${part1(input)}")
-    println("Part 2: ${part2(input)}")
+//    println("Part 2: ${part2(input)}")
+}
+
+data class Lanternfish(
+    var timer: Int = 8,
+    var firstCicle: Boolean = true
+) {
+    fun reset() {
+        timer = 6
+        firstCicle = false
+    }
 }
